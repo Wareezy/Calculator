90b1386ea118b9f1bde44cdc9b9a2d8b55102ac3
Warren <warrenjaftha16@gmail.com>
2021-03-11 16:23:06 +0200

    Modified the integerSum() Added doubleSum()

--- src/za/ac/cput/Addition.java
+++ src/za/ac/cput/Addition.java
@@ -10,9 +10,19 @@ package za.ac.cput;
  * @author warre
  */
 public class Addition {
+    /**
+     * This adds two integers 
+     * @param a
+     * @param b
+     * @return 
+     */
     public static int integerSum(int a, int b)
     {return  a+b;
         
     }
-    
+    /**
+     * This adds two real numbers
+     */
+    public static double doubleSum(double a, double b)
+    {return a+b;}
 }
